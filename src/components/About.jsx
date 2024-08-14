a2c85a31f1e6a605fe53f82dd58d7f981101c4b7
Abdusamadbek Akhamdjonov <axmadjonov.ti@gmail.com>
2024-08-14 16:23:15 +0500

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,14 +4,14 @@ import { BsCopy } from "react-icons/bs";
 
 function About() {
   return (
-    <div className="lg:w-10/12 h-full jetbrains font-extralight  lg:text-base about overflow-auto">
+    <div className="lg:w-10/12 h-full jetbrains font-extralight text-sm lg:text-base about overflow-auto">
       {/* <InteractiveBackground /> */}
       <div className="flex flex-col lg:flex-row w-full h-full p-5 lg:p-20 overflow-auto">
         {/* first side */}
-        <div className="h-full w-full p-2 flex flex-col gap-10 overflow-visible">
+        <div className="h-full w-full p-2 flex flex-col gap-5 lg:gap-10 overflow-visible">
           <div className="lg:w-2/3 flex flex-col gap-2 overflow-visible">
             <div className="text-2xl">Hello there! </div>
-            <div className="overflow-visible">
+            <div className="overflow-visible text-base">
               My name is Abdusamadbek Akhmadjonov. I'm a UX/UI designer and
               full-stack developer based in Uzbekistan. My passion for
               technology drives me to continually explore and learn new aspects
@@ -19,8 +19,9 @@ function About() {
               building effective solutions, always eager to expand my knowledge.
             </div>
           </div>
+          <hr className=" lg:hidden" />
           <div className="flex flex-col gap-3">
-            <div className="text-2xl">Languages</div>
+            <div className="text-xl lg:text-2xl">Languages</div>
             <div className="flex gap-5">
               <div className="flex flex-col gap-3">
                 <div>Uzbek</div>
@@ -33,17 +34,17 @@ function About() {
             </div>
           </div>
           <div className="flex flex-col gap-3">
-            <div className="text-2xl">
+            <div className="text-lg lg:text-2xl">
               Let's connect{" "}
               <span className="font-bold text-gradient">[AVAILABLE]</span>{" "}
             </div>
             <div className="flex gap-8 links-container ">
-              <div className="flex flex-col gap-3 links-name-container">
+              <div className="flex lg:flex-col gap-6 lg:gap-3 links-name-container">
                 <div className="cursor-pointer email-link">Email</div>
                 <div className="cursor-pointer github-link">Github</div>
                 <div className="cursor-pointer cv-link h">CV</div>
               </div>
-              <div className="flex flex-col items-start gap-3 overflow-hidden">
+              <div className="lg:flex hidden flex-col items-start gap-3 overflow-hidden">
                 <div className="cursor-pointer overflow-hidden email-arrow">
                   [copy]
                 </div>
@@ -54,6 +55,7 @@ function About() {
           </div>
         </div>
         {/* second side */}
+        <hr className="my-5 lg:hidden" />
         <div className="h-full w-full lg:w-2/3 p-2 flex flex-col justify-between gap-10">
           <div className="">Senior Software Engineering student.</div>
           <div className="h-full flex flex-wrap gap-x-10">
