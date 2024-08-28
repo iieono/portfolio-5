b7ee04132add59251d579b5f1680969babc8af2a
Abdusamadbek Akhamdjonov <axmadjonov.ti@gmail.com>
2024-08-28 22:18:57 +0500

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,69 +16,12 @@ function About() {
               of the field. I am dedicated to creating engaging designs and
               building effective solutions, always eager to expand my knowledge.
             </div>
-            <div className="bg-lead rounded-full blur-xl w-40 h-40 self-center"></div>
+            <div className="bg-lead rounded-full lg:mt-10 blur-xl w-40 h-40 self-center"></div>
           </div>
-          {/* <div className="flex flex-col gap-3">
-            <div className="text-lg lg:text-2xl">
-              Let's connect{" "}
-              <span className="font-bold text-gradient">[AVAILABLE]</span>{" "}
-            </div>
-            <div className="flex gap-8 links-container ">
-              <div className="flex lg:flex-col gap-6 lg:gap-3 links-name-container">
-                <div className="cursor-pointer email-link">Email</div>
-                <div className="cursor-pointer github-link">Github</div>
-                <div className="cursor-pointer cv-link h">CV</div>
-              </div>
-              <div className="lg:flex hidden flex-col items-start gap-3 overflow-hidden">
-                <div className="cursor-pointer overflow-hidden email-arrow">
-                  [copy]
-                </div>
-                <div className="cursor-pointer overflow-hidden github-arrow">{`->`}</div>
-                <div className="cursor-pointer overflow-hidden cv-arrow h-max">{`->`}</div>
-              </div>
-            </div>
-          </div> */}
         </div>
         {/* second side */}
-        <div className="h-4/5 w-full lg:w-2/3 p-2 flex flex-col items-end justify-end gap-10 overflow-visible">
-          <div className="flex h-full flex-wrap gap-x-10 pr-2">
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-            <div className="skill-item">Nextjs</div>
-          </div>
-          <div className="flex h-full flex-wrap pr-2 gap-x-10">
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-            <div className="skill-item">Blender</div>
-          </div>
-          <div className="h-full overflow-visible pr-2">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
-            quos repellendus amet laudantium tempore ipsum. Cum pariatur,
-            eveniet perspiciatis ullam est doloremque consequuntur, neque
-            quaerat nemo voluptatum laborum voluptas soluta. Nihil consequatur
-            vitae fugit deleniti maxime, obcaecati iusto numquam est.
-          </div>
+        <div className="h-[70%] lg:h-[70%] min-h-[70%] w-full lg:w-2/3 p-2 flex flex-col self-end items-end justify-end gap-10 overflow-visible">
+          
         </div>
       </div>
     </div>
